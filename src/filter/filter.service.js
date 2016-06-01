8b25fe2e1aa4b20c1b4907ded28033f6f6d2556f
cablej <jackhcable@gmail.com>
2016-06-01 12:00:35 -0500

    Current with SnapJS

--- src/filter/filter.service.js
+++ src/filter/filter.service.js
@@ -5,12 +5,13 @@ class Filter {
   constructor() {
     this.operators = {
       String: ['equals', 'not equal', 'like'],
-      Number: ['equals', 'not equal', 'less than', 'greater than', 'less than or equal to',
-        'greater than or equal to'],
+      Number: ['equals', 'not equal', 'less than', 'greater than',
+               'less than or equal to', 'greater than or equal to'],
       Boolean: ['true', 'false'],
       Date: ['less than', 'greater than', 'less than or equal to', 'greater than or equal to'],
       ObjectID: ['equals', 'not equal', 'like'],
-      Image: ['equals', 'not equal', 'like']
+      Image: ['equals', 'not equal', 'like'],
+      Array: ['equals', 'not equal', 'like']
     };
   }
 
